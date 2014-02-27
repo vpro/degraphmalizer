c5845f3db08fcd3af0e8cf274994201f0e1aa618
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-02-27 13:46:10 +0100

    I love @InjectLogger

--- degraphmalizer-core/src/main/java/dgm/driver/Main.java
+++ degraphmalizer-core/src/main/java/dgm/driver/Main.java
@@ -171,7 +171,7 @@ public final class Main {
         final int port = Integer.parseInt(opt.transport.get(1));
         final String bindhost = opt.bindhost;
 
-        log.info("Connecting to ES cluster:" + cluster + " bindhost: " + bindhost + " host: " + host + " port:" + port);
+        System.out.println("Connecting to ES cluster:" + cluster + " bindhost: " + bindhost + " host: " + host + " port:" + port);
 
         modules.add(new NodeES(cluster, bindhost, host, port));
     }
